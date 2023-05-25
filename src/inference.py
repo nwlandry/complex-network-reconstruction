dcf64f2548ef4858e475f1a5e345395b8b4d6e83
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-05-25 18:34:20 -0400

    update

--- src/inference.py
+++ src/inference.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import xgi
 from scipy import sparse
+from numpy import ndarray
 from scipy.stats import beta
 
 
@@ -28,13 +29,16 @@ def infer_adjacency_matrix(
     # This assumes a uniform prior on hypergraphs with a given rho.
 
     # form initial contact structure. Number of patients (n) and rooms (m) is fixed.
+    if not isinstance(A0, ndarray):
+        A0 = A0.todense()
+
     A = A0.copy()
     n, m = np.shape(A)
 
     if n != m:
         Exception("Matrix must be square!")
 
-    samples = []
+    samples = np.zeros((nsamples, n, n))
 
     l_node = np.zeros(n)
 
@@ -48,12 +52,17 @@ def infer_adjacency_matrix(
     l_adjacency = adjacency_log_likelihood(num_entries, n, m, rho)
 
     accept = 0
-    it = 0
-    s_i = 0
-    while it < burn_in + (nsamples - 1) * skip:
+    it = 1
+    s_i = skip
+
+    samples = np.zeros((nsamples, n, n))
+    print(np.shape(samples))
+    sample_num = 0
+
+    while it <= burn_in + (nsamples - 1) * skip:
 
         # proposal comes from the lower triangle
-        i = random.randrange(n)
+        i = random.randrange(1, n)
         j = random.randrange(i)
 
         # alter hypergraph
@@ -90,17 +99,21 @@ def infer_adjacency_matrix(
 
         l_vals.append(l)
 
-        if it + 1 >= burn_in:
-            if s_i >= skip - 1:
-                samples.append(A.copy())
-                s_i = 0
+        if it >= burn_in:
+            if s_i >= skip:
+                samples[sample_num, :, :] = A.copy()
+                sample_num += 1
+                s_i = 1
             else:
                 s_i += 1
 
         it += 1
     print(f"Acceptance ratio is {accept/(burn_in + (nsamples - 1)*skip)}")
 
-    return samples, l_vals if return_likelihoods else samples
+    if return_likelihoods:
+        return samples, l_vals
+    else:
+        return samples
 
 
 def compute_delta(a, b):
@@ -114,7 +127,7 @@ def node_log_likelihood(x, v, A, beta, f):
     l = 0
     T = np.size(x, axis=0)
     for t in range(T - 1):
-        infected_nbrs = A[v] @ x
+        infected_nbrs = A[v] @ x[t]
         p_x = beta * f(infected_nbrs)
         if p_x > 0 and p_x < 1:
             lt = (1 - x[t, v]) * x[t + 1, v] * np.log(p_x) + (1 - x[t, v]) * (
@@ -156,9 +169,13 @@ def update_adjacency_matrix(i, j, A):
         return -2
 
 
-def adjacency_matrix_prior(n, m, rho):
+def adjacency_matrix_prior(n, rho):
     # n is the number of people, m is number of rooms
-    return np.random.choice([0, 1], size=(n, m), p=[1 - rho, rho])
+    A = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i):
+            A[i, j] = A[j, i] = 1
+    return A
 
 
 def infer_dynamics(x, y, H, f, g):
