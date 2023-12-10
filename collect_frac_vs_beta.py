f1342cc5e8a95a4bdaf06b19f0ee240cb4845a06
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-12-10 11:42:31 -0500

    update to parallelize

--- collect_frac_vs_beta.py
+++ collect_frac_vs_beta.py
@@ -1,82 +1,92 @@
 import json
 import os
 
-import networkx as nx
 import numpy as np
+from joblib import Parallel, delayed
 
 from lcs import *
 
-mean_infections = []
-ps = []
-fraclist = set()
-betalist = set()
-rlist = set()
-beta = []
-frac = []
+data_dir = "Data/erdos-renyi/"
 
-A = nx.adjacency_matrix(nx.karate_club_graph()).todense()
 
-data_dir = "Data/frac_vs_beta/"
-for f in os.listdir(data_dir):
-    d = f.split(".json")[0].split("-")
-    beta = float(d[0])
-    frac = float(d[1])
+def collect_parameters(dir):
+    blist = set()
+    flist = set()
+    rlist = set()
+
+    for f in os.listdir(dir):
+        d = f.split(".json")[0].split("_")
+
+        b = int(d[0])
+        f = float(d[1])
+        r = int(d[2])
+
+        blist.add(b)
+        flist.add(f)
+        rlist.add(r)
+
+    b_dict = {b: i for i, b in enumerate(sorted(blist))}
+    f_dict = {p: i for i, p in enumerate(sorted(flist))}
+    r_dict = {r: i for i, r in enumerate(sorted(rlist))}
+
+    return b_dict, f_dict, r_dict
+
+
+def get_metrics(f, dir, b_dict, f_dict, r_dict):
+    fname = os.path.join(dir, f)
+    d = f.split(".json")[0].split("_")
+    b = int(d[0])
+    f = float(d[1])
     r = int(d[2])
 
-    fraclist.add(frac)
-    betalist.add(beta)
-    rlist.add(r)
-
-fraclist = sorted(fraclist)
-betalist = sorted(betalist)
-rlist = sorted(rlist)
-
-frac_dict = dict(zip(fraclist, range(len(fraclist))))
-beta_dict = dict(zip(betalist, range(len(betalist))))
-r_dict = dict(zip(rlist, range(len(rlist))))
-
-nf = len(fraclist)
-nb = len(betalist)
-nr = len(rlist)
-print((nf, nb, nr), flush=True)
-
-psmat = np.zeros((nf, nb, nr))
-spsmat = np.zeros((nf, nb, nr))
-ipn = np.zeros((nf, nb, nr))
-
-it = 0
-for f in os.listdir(data_dir):
-    d = f.split(".json")[0].split("-")
-    beta = float(d[0])
-    frac = float(d[1])
-    r = int(d[2])
-
-    i = frac_dict[frac]
-    j = beta_dict[beta]
+    i = b_dict[b]
+    j = f_dict[f]
     k = r_dict[r]
-
-    fname = os.path.join(data_dir, f)
 
     with open(fname, "r") as file:
         data = json.loads(file.read())
 
-    x = np.array(data["x"])
-    # A = np.array(data["A"])
+    A = np.array(data["A"])
     samples = np.array(data["samples"])
 
-    ipn[i, j, k] = infections_per_node(x)
+    ps = posterior_similarity(samples, A)
+    sps = samplewise_posterior_similarity(samples, A)
+    fc = fraction_of_correct_entries(samples, A)
+    print((i, j, k), flush=True)
 
-    psmat[i, j, k] = posterior_similarity(samples, A)
-    spsmat[i, j, k] = samplewise_posterior_similarity(samples, A)
-    it += 1
-    print(it, flush=True)
+    return i, j, k, ps, sps, fc
+
+
+# get number of available cores
+n_processes = len(os.sched_getaffinity(0))
+
+b_dict, f_dict, r_dict = collect_parameters(data_dir)
+
+n_b = len(b_dict)
+n_f = len(f_dict)
+n_r = len(r_dict)
+
+ps = np.zeros((n_f, n_b, n_r))
+sps = np.zeros((n_f, n_b, n_r))
+fce = np.zeros((n_f, n_b, n_r))
+
+arglist = []
+for f in os.listdir(data_dir):
+    arglist.append((f, data_dir, b_dict, f_dict, r_dict))
+
+data = Parallel(n_jobs=n_processes)(delayed(get_metrics)(*arg) for arg in arglist)
+
+for i, j, k, pos_sim, s_pos_sim, frac_corr in data:
+    ps[i, j, k] = pos_sim
+    sps[i, j, k] = s_pos_sim
+    fce[i, j, k] = frac_corr
 
 data = {}
-data["fraction"] = fraclist
-data["beta"] = betalist
-data["sps"] = spsmat.tolist()
-data["ps"] = psmat.tolist()
-data["ipn"] = ipn.tolist()
+data["fraction"] = list(f_dict)
+data["beta"] = list(b_dict)
+data["sps"] = sps.tolist()
+data["ps"] = ps.tolist()
+data["fce"] = fce.tolist()
 
 datastring = json.dumps(data)
 
