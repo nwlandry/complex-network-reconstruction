b6dd8618f93c4a217c552c1e69d3392aba5914d7
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2024-04-08 22:50:55 -0400

    Fig 1 updates

--- plot_fig1.py
+++ plot_fig1.py
@@ -10,11 +10,11 @@ from matplotlib.gridspec import GridSpec
 import fig_settings as fs
 from lcs import *
 
-axislabel_fontsize = 20
-tick_fontsize = 18
+axislabel_fontsize = 12
+tick_fontsize = 12
 fs.set_fonts(
     {
-        "font.family": "sans-serif",
+        "font.family": "serif",
         "axes.labelsize": axislabel_fontsize,
         "xtick.labelsize": tick_fontsize,
         "ytick.labelsize": tick_fontsize,
@@ -23,15 +23,17 @@ fs.set_fonts(
 fs.set_colors()
 cmap = fs.cmap
 
-fig = plt.figure(figsize=(8, 6))
-plt.subplots_adjust(left=0.1, right=0.86, bottom=0.15, top=0.95, wspace=0.4, hspace=0.4)
+fig = plt.figure(figsize=(7, 5))
+plt.subplots_adjust(left=0.1, right=0.86, bottom=0.15, top=0.95)
 
-gs = GridSpec(2, 2, hspace=0.4, wspace=0.4)
+gs = GridSpec(2, 2, hspace=0.6, wspace=0.5)
 
 """
 Panel 1: Network Viz
 """
 ax1 = fig.add_subplot(gs[0])
+
+ax1.text(-0.29, 1.1, "(a)", transform=ax1.transAxes, fontsize=13, fontweight='bold', va="top")
 
 el = zkc(format="edgelist")
 H = xgi.Hypergraph(el)
@@ -53,7 +55,7 @@ x = contagion_process(A, gamma, c, x0, tmin=0, tmax=100, random_seed=2)
 infected_color = "C1"
 susceptible_color = "white"
 subgraph_color = "black"
-graph_color = (0.1, 0.1, 0.1, 0.1)
+graph_color = (0.7, 0.7, 0.7, 0.5)
 subgraph_node_lc = "black"
 graph_node_lc = (0.3, 0.3, 0.3)
 
@@ -61,7 +63,12 @@ sg = H.nodes.memberships(i)
 nbrs = H.nodes.neighbors(i)
 nbrs.add(i)
 
-pos = xgi.pca_transform(xgi.pairwise_spring_layout(H, seed=5, k=0.3))
+pos = xgi.pca_transform(xgi.pairwise_spring_layout(H, seed=5, k=0.25))
+# rotate pos by 30 degree3
+angle = -1.15 * np.pi / 6
+rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+pos = {k: np.dot(v, rotation_matrix) for k,v in pos.items()}
+
 node_fc = [infected_color if x[t, i] else susceptible_color for i in H.nodes]
 node_ec = [subgraph_node_lc if n in nbrs else graph_node_lc for n in H.nodes]
 node_fc[12] = "C0"
@@ -75,17 +82,22 @@ xgi.draw(
     node_size=6.5,
     node_fc=node_fc,
     dyad_color=dyad_color,
-    dyad_lw=0.5,
+    dyad_lw=0.8,
     node_ec=node_ec,
-    node_lw=0.5,
-    ax=ax1,
+    node_lw=0.8,
+    ax=ax1
 )
+plt.scatter(pos[13][0], pos[13][1], s=50, c='C0', edgecolors='black', linewidths=0.8, zorder=10, marker='s')
+
+ax1.set_xlim([1.01 * min([pos[i][0] for i in pos]), 1.01 * max([pos[i][0] for i in pos])])
 
 
 """
 Panel 2: 
 """
 ax2 = fig.add_subplot(gs[1])
+ax2.text(-0.3, 1.1, "(b)", transform=ax2.transAxes, fontsize=12, fontweight='bold',  va="top")
+
 
 with open("Data/zkc_infer_contagion_functions.json") as file:
     data = json.load(file)
@@ -110,7 +122,7 @@ nus = np.arange(0, n, 1)
 
 # simple contagion
 c1_mean = c1_samples.mean(axis=0)
-ax2.plot(nus, c1, "-", color="C0", label="Simple contagion")
+ax2.plot(nus, c1, "-", color="C0", lw=5, alpha=0.5)
 
 err_c1 = np.zeros((2, n))
 c1_mode = np.zeros(n)
@@ -119,11 +131,15 @@ for i in range(n):
     x, y = interval
     err_c1[0, i] = max(c1_mean[i] - x, 0)
     err_c1[1, i] = max(y - c1_mean[i], 0)
-ax2.errorbar(nus, c1_mean, err_c1, color="C0", fmt="o")
+
+    
+offset_distance = 0.15
+ax2.errorbar(nus - offset_distance, c1_mean, err_c1, color="C0", fmt="o",
+             capsize=3, markersize=5, markeredgecolor="#315b7d", label="Simple")
 
 # threshold contagion, tau=2
 c2_mean = c2_samples.mean(axis=0)
-ax2.plot(nus, c2, "-", color="C1", label="Complex contagion")
+ax2.plot(nus, c2, "-", color="C1", lw=5, alpha=0.5)
 
 err_c2 = np.zeros((2, n))
 c2_mode = np.zeros(n)
@@ -132,17 +148,19 @@ for i in range(n):
     x, y = interval
     err_c2[0, i] = max(c2_mean[i] - x, 0)
     err_c2[1, i] = max(y - c2_mean[i], 0)
-ax2.errorbar(nus, c2_mean, err_c2, color="C1", fmt="o")
+ax2.errorbar(nus + offset_distance, c2_mean, err_c2, color="C1", fmt="o",
+             capsize=3, markersize=5, markeredgecolor="#391c23", label="Complex")
+
 
 ax2.set_xticks(np.arange(0, n, 5))
-ax2.set_xlabel(r"$\nu$")
-ax2.set_ylabel(r"$c(\nu)$")
+ax2.set_xlabel(r"# of infected neighbors $\nu$")
+ax2.set_ylabel(r"Contagion $c(\nu)$")
 
-ax2.set_xlim([0, kmax + 2.5])
+ax2.set_xlim([0, 15.5])
 ax2.set_ylim([0, 1])
 ax2.set_yticks([0, 0.5, 1], [0, 0.5, 1])
 
-ax2.legend(loc="upper left")
+ax2.legend(loc="upper left", handletextpad=0.1, frameon=False)
 
 sns.despine()
 
@@ -150,6 +168,7 @@ sns.despine()
 Panel 3: recovery vs. tmax
 """
 ax3 = fig.add_subplot(gs[2])
+ax3.text(-0.29, 1.1, "(c)", transform=ax3.transAxes, fontsize=12, fontweight='bold',  va="top")
 
 
 with open("Data/zkc_infer_vs_tmax.json") as file:
@@ -162,14 +181,15 @@ ps = np.array(data["ps"], dtype=float)
 fce = np.array(data["fce"], dtype=float)
 
 
-ax3.semilogx(tmax, sps[0].mean(axis=1), color="C0", label="Simple contagion")
-ax3.semilogx(tmax, sps[1].mean(axis=1), color="C1", label="Complex contagion")
+ax3.semilogx(tmax, sps[0].mean(axis=1), color="C0", label="Simple")
+ax3.semilogx(tmax, sps[1].mean(axis=1), color="C1", label="Complex")
 ax3.fill_between(
     tmax,
     sps[0].mean(axis=1) - sps[0].std(axis=1),
     sps[0].mean(axis=1) + sps[0].std(axis=1),
     alpha=0.3,
     color="C0",
+    edgecolor="none"
 )
 ax3.fill_between(
     tmax,
@@ -177,6 +197,7 @@ ax3.fill_between(
     sps[1].mean(axis=1) + sps[1].std(axis=1),
     alpha=0.3,
     color="C1",
+    edgecolor="none"
 )
 ax3.set_ylabel("Performance")
 ax3.set_xlabel(r"$t_{max}$")
@@ -188,13 +209,14 @@ ax3.set_xticks(
 ax3.set_ylim([0, 1])
 ax3.set_yticks([0, 0.5, 1], [0, 0.5, 1])
 
-ax3.legend(loc="upper left")
+ax3.legend(loc="lower right", markerfirst=False, frameon=False, handlelength=0.8)
 sns.despine()
 
 """
 Panel 4: heatmap of recover vs. beta and f
 """
 ax4 = fig.add_subplot(gs[3])
+ax4.text(-0.31, 1.1, "(d)", transform=ax4.transAxes, fontsize=12, fontweight='bold',  va="top")
 
 with open("Data/zkc_frac_vs_beta.json") as file:
     data = json.load(file)
@@ -217,16 +239,16 @@ c = ax4.imshow(
     vmin=0,
     vmax=1,
 )
-ax4.set_xlabel(r"$f$")
-ax4.set_ylabel(r"$\beta$")
+ax4.set_xlabel(r"Complexity $\lambda$")
+ax4.set_ylabel(r"Infectivity $\beta$")
 
 ax4.set_xticks([0, 0.5, 1], [0, 0.5, 1])
 ax4.set_yticks([0, 0.5, 1], [0, 0.5, 1])
 
 
-cbar_ax = fig.add_axes([0.875, 0.15, 0.015, 0.335])  # x, y, width, height
+cbar_ax = fig.add_axes([0.875, 0.15, 0.015, 0.31])  # x, y, width, height
 cbar = plt.colorbar(c, cax=cbar_ax)
-cbar.set_label(r"Performance", fontsize=axislabel_fontsize, rotation=270, labelpad=25)
+cbar.set_label(r"Performance", fontsize=axislabel_fontsize, rotation=270, labelpad=15)
 cbar_ax.set_yticks([0, 0.5, 1], [0, 0.5, 1], fontsize=tick_fontsize)
 
 plt.savefig("Figures/Fig1/fig1.png", dpi=1000)
