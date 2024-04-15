796903fad787d9b96127a9f80fea6d7f41d436f3
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-04-15 14:40:58 -0400

    updated script

--- plot_fig2.py
+++ plot_fig2.py
@@ -8,52 +8,32 @@ from matplotlib.gridspec import GridSpec
 import fig_settings as fs
 from lcs import *
 
-metric_name = "fce-norm-random"
-axis_limits = [0, 2]
+axis_limits = [0, 1]
 
-axislabel_fontsize = 20
-tick_fontsize = 18
-fs.set_fonts(
-    {
-        "font.family": "sans-serif",
-        "axes.labelsize": axislabel_fontsize,
-        "xtick.labelsize": tick_fontsize,
-        "ytick.labelsize": tick_fontsize,
-    }
-)
+fs.set_fonts()
 fs.set_colors()
 cmap = fs.cmap
 
 
-models = ["Erdos-Renyi", "CM", "clustered_network", "SBM", "Watts-Strogatz"]
+models = ["Erdos-Renyi", "CM", "clustered_network",]
 cfs = [
     "SIS",
     r"Threshold, $\tau=2$",
-    r"Threshold, $\tau=3$",
 ]
-keys = ["p", "alpha", "size", "epsilon", "p"]
-titles = ["Erdös-Rényi", "Power-law CM", "Clustered", "SBM", "Small-World"]
-labels = [r"$p$", r"$\alpha$", r"$s$", r"$\epsilon$", r"$p$"]
+keys = ["p", "alpha", "size"]
+titles = ["Erdös-Rényi", "Power-law CM", "Clustered"]
+labels = [r"$p$", r"$\alpha$", r"$s$"]
 xticks = [
     [0, 0.5, 1],
     [-4, -3.5, -3, -2.5, -2, -1.5],
     [1, 7, 13, 19],
-    [0, 0.5, 1],
-    [-6, -4, -2, 0],
 ]
 xticklabels = [
     ["0", "0.5", "1"],
     ["-4", "-3.5", "-3", "-2.5", "-2", "-1.5"],
     ["1", "7", "13", "19"],
-    ["0", "0.5", "1"],
-    [
-        r"$\mathregular{10^{-6}}$",
-        r"$\mathregular{10^{-2}}$",
-        r"$\mathregular{10^{-2}}$",
-        r"$\mathregular{10^{0}}$",
-    ],
 ]
-convert_to_log = [False, False, False, False, True]
+convert_to_log = [False, False, False]
 
 
 def visualize_networks(i, ax):
@@ -73,12 +53,6 @@ def visualize_networks(i, ax):
             k2 = clique_size * np.ones(num_cliques)
             A = clustered_network(k1, k2, seed=0)
             e = [(i, j) for i, j in nx.Graph(A).edges]
-        case 3:
-            A = sbm(n, 10, 0.9, seed=0)
-            e = [(i, j) for i, j in nx.Graph(A).edges]
-        case 4:
-            A = watts_strogatz(n, 6, 0.03, seed=0)
-            e = [(i, j) for i, j in nx.Graph(A).edges]
 
     H = xgi.Hypergraph(e)
 
@@ -93,36 +67,56 @@ def visualize_networks(i, ax):
             pos = xgi.pairwise_spring_layout(H, seed=2)
         case 2:
             pos = xgi.pairwise_spring_layout(H, seed=2)
-        case 3:
-            pos = xgi.pca_transform(xgi.pairwise_spring_layout(H, seed=2))
-        case 4:
-            pos = xgi.circular_layout(H)
     xgi.draw(H, ax=ax, pos=pos, node_size=node_size, node_lw=node_lw, dyad_lw=dyad_lw)
 
 
-fig = plt.figure(figsize=(16, 10))
-plt.subplots_adjust(left=0.09, right=0.9, bottom=0.1, top=0.95, wspace=0.4, hspace=0.4)
+fig = plt.figure(figsize=(8, 8))
+plt.subplots_adjust(left=0.12, right=0.85, bottom=0.1, top=0.95, wspace=0.4, hspace=0.4)
 
-gs = GridSpec(len(cfs) + 1, len(models), wspace=0.2, hspace=0.2)
+gs = GridSpec(2 * len(cfs) + 1, len(models), wspace=0.2, hspace=0.2)
 
 for i, m in enumerate(models):
     with open(f"Data/{m.lower()}.json") as file:
         data = json.load(file)
     var = np.array(data[keys[i]], dtype=float)
     b = np.array(data["beta"], dtype=float)
-    recovery_metric = np.array(data[metric_name], dtype=float)
 
-    if convert_to_log[i]:
-        var = np.log10(var)
+    recovery_metric = np.array(data["auprc"], dtype=float)
 
+    # plot the auprc
     for j, cf in enumerate(cfs):
         recovery_average = recovery_metric[j].mean(axis=2).T
         ax = fig.add_subplot(gs[j + 1, i])
-        im = ax.imshow(
+        im1 = ax.imshow(
             to_imshow_orientation(recovery_average),
             extent=(min(var), max(var), min(b), max(b)),
             vmin=axis_limits[0],
             vmax=axis_limits[1],
+            aspect="auto",
+            cmap=cmap,
+        )
+        ax.set_xlim([min(var), max(var)])
+        ax.set_ylim([min(b), max(b)])
+        ax.set_xticks(xticks[i], xticklabels[i])
+        ax.set_yticks([0, 0.5, 1], [0, 0.5, 1])
+
+        if i == 0:
+            ax.set_ylabel(f"{cfs[j]}\n" + r"$\beta$")
+        else:
+            ax.set_yticks([], [])
+
+        ax.set_xticks([], [])
+
+    # plot the density
+    recovery_metric = np.array(data["rho"], dtype=float) - np.array(data["rho-samples"], dtype=float)
+    for j, cf in enumerate(cfs):
+        recovery_average = recovery_metric[j].mean(axis=2).T
+        ax = fig.add_subplot(gs[j + 3, i])
+        im2 = ax.imshow(
+            to_imshow_orientation(recovery_average),
+            extent=(min(var), max(var), min(b), max(b)),
+            vmin=-1,
+            vmax=1,
             aspect="auto",
             cmap=cmap,
         )
@@ -141,10 +135,15 @@ for i, m in enumerate(models):
         else:
             ax.set_xticks([], [])
 
-cbar_ax = fig.add_axes([0.91, 0.1, 0.015, 0.63])
-cbar = fig.colorbar(im, cax=cbar_ax)
-cbar.set_label(r"Performance", fontsize=axislabel_fontsize, rotation=270, labelpad=25)
-cbar_ax.set_yticks([0, 0.5, 1, 1.5, 2], [0, 0.5, 1, 1.5, 2], fontsize=tick_fontsize)
+cbar_ax1 = fig.add_axes([0.86, 0.45, 0.015, 0.325])
+cbar = fig.colorbar(im1, cax=cbar_ax1)
+cbar.set_label(r"AUPRC", rotation=270, labelpad=25)
+cbar_ax1.set_yticks([0, 0.5, 1], [0, 0.5, 1])
+
+cbar_ax2 = fig.add_axes([0.86, 0.1, 0.015, 0.325])
+cbar = fig.colorbar(im2, cax=cbar_ax2)
+cbar.set_label(r"Density difference", rotation=270, labelpad=25)
+cbar_ax2.set_yticks([-1, 0, 1], [-1, 0, 1])
 
 for i, m in enumerate(models):
     ax = fig.add_subplot(gs[0, i])
@@ -152,6 +151,7 @@ for i, m in enumerate(models):
     ax.set_title(titles[i])
 
 
-plt.savefig(f"Figures/Fig2/generative_models_{metric_name}.png", dpi=1000)
-plt.savefig(f"Figures/Fig2/generative_models_{metric_name}.pdf", dpi=1000)
-# plt.show()
+plt.savefig(f"test.pdf", dpi=1000)
+# plt.savefig(f"Figures/Fig2/generative_models_{metric_name}.png", dpi=1000)
+# plt.savefig(f"Figures/Fig2/generative_models_{metric_name}.pdf", dpi=1000)
+plt.show()
